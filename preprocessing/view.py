e07a8a8d82b7c5a13da3624a2aacedf8d0cb9556
Andrew Houghton <andrew.houghton@mediushealth.org>
2020-02-03 08:46:33 +1100

    Move preprocessing code to preprocessing directory

---
+++ preprocessing/view.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import numpy
+from pathlib import Path
+
+
+data_folder = Path(__file__).absolute().parent.parent.joinpath('data')
+print("Loading file")
+im = Image.open(data_folder.joinpath('ASTGTMV003_S45E168_dem.tif'))
+imarray = numpy.array(im)
+imarray = imarray//(imarray.max()/256)  # // is floor division operator
+imarray = imarray.astype('int32')
+im2 = Image.fromarray(imarray)
+print("Displaying file")
+im2.show()
+print("Saving file")
+im2.convert("RGB").save(data_folder.joinpath('ASTGTMV003_S45E168_dem.png'))
