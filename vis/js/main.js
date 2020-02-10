7b0ef1dc01efee57b052a92a03cdc99b28e8bcbe
Andrew Houghton <andrew.houghton@mediushealth.org>
2020-02-10 16:49:39 +1100

    Forward and back running

--- vis/js/main.js
+++ vis/js/main.js
@@ -1,23 +1,75 @@
 var currentState = 0;
-var maxState=10;
+var maxState = 10;
 var svg;
+var isStateLocked = false;
 
-function transitionToState(stateIndex, previousState) {
-    if (stateIndex == 1 && previousState == 0){
-        true_colour = svg.getElementById("true_colour");
-
-        var animageFade = document.createElementNS('http://www.w3.org/2000/svg','animate');
-        animageFade.setAttribute("attributeName","opacity");
-        animageFade.setAttribute("begin","indefinite");
-        animageFade.setAttribute("dur","5s");
-        animageFade.setAttribute("values","1;0");
-        animageFade.setAttribute("fill","freeze");
-        true_colour.appendChild(animageFade)
-        animageFade.beginElement()
+function getFadeElement(fadeOut, id = null) {
+    var animateFade = document.createElementNS('http://www.w3.org/2000/svg', 'animate');
+    animateFade.setAttribute("attributeName", "opacity");
+    animateFade.setAttribute("begin", "indefinite");
+    animateFade.setAttribute("dur", "5s");
+    animateFade.setAttribute("values", fadeOut ? "1;0" : "0;1");
+    animateFade.setAttribute("fill", "freeze");
+    if (fadeOut) {
+        animateFade.setAttribute("onend", "removeElement('" + id + "')");
     }
-    console.log("Changing to state "+ stateIndex)
+    return animateFade
 }
 
+function getImageElement(url, id) {
+    image = document.createElementNS('http://www.w3.org/2000/svg', 'image');
+    image.setAttribute("id", id)
+    image.setAttribute("href", url)
+    image.setAttribute("preserveAspectRatio", "xMidYMid meet")
+    image.setAttribute("width", "100")
+    image.setAttribute("height", "100")
+    return image
+}
+
+function swapImagesBack() {
+    trueColour = svg.getElementById("heightMap");
+    fadeOut = getFadeElement(true, "heightMap")
+    trueColour.appendChild(fadeOut)
+
+    heightMap = getImageElement("img/true_colour_resized.jpg", "trueColour")
+    fadeIn = getFadeElement(false)
+    heightMap.appendChild(fadeIn)
+    svg.appendChild(heightMap)
+
+    fadeIn.beginElement()
+    fadeOut.beginElement()
+}
+
+function swapImages() {
+    trueColour = svg.getElementById("trueColour");
+    fadeOut = getFadeElement(true, "trueColour")
+    trueColour.appendChild(fadeOut)
+
+    heightMap = getImageElement("img/height_map.png", "heightMap")
+    fadeIn = getFadeElement(false)
+    heightMap.appendChild(fadeIn)
+    svg.appendChild(heightMap)
+
+    fadeIn.beginElement()
+    fadeOut.beginElement()
+}
+
+function transitionToState(stateIndex, previousState) {
+    if (stateIndex == 1 && previousState == 0) {
+        isStateLocked = true;
+        swapImages()
+    } else if (stateIndex == 0 && previousState == 1) {
+        isStateLocked = true;
+        swapImagesBack()
+    }
+    console.log("Changing to state " + stateIndex)
+}
+
+function removeElement(elementId) {
+    trueColour = svg.getElementById(elementId);
+    svg.removeChild(trueColour);
+    isStateLocked = false;
+}
 
 document.addEventListener("DOMContentLoaded", function(event) {
     svg = document.getElementById("vis");
@@ -25,14 +77,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 function nextState() {
-    if (currentState < maxState){
+    if (currentState < maxState && !isStateLocked) {
         currentState++;
         transitionToState(currentState, currentState - 1)
     }
 }
 
 function previousState() {
-    if (currentState > 0){
+    if (currentState > 0 && !isStateLocked) {
         currentState--;
         transitionToState(currentState, currentState + 1)
     }
