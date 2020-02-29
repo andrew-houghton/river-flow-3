581a22c6f8f3147cf81e6e8d2c5d68679607e5b0
Andrew Houghton <houghtonandrew0@gmail.com>
2020-02-29 17:58:02 +1100

    Factor out events

--- pygame/interface.py
+++ pygame/interface.py
@@ -7,6 +7,20 @@ def get_image(filename):
     image = Image.open(Path(__file__).absolute().parent.parent.joinpath("data", filename))
     return pygame.image.fromstring(image.tobytes(), image.size, image.mode)
 
+
+def handle_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                return False
+            elif event.key == pygame.K_LEFT:
+                pass
+            elif event.key == pygame.K_RIGHT:
+                pass
+    return True
+
 pygame.init()
 infoObject = pygame.display.Info()
 size = (infoObject.current_w, infoObject.current_h)
@@ -24,15 +38,5 @@ pygame.display.flip()
 
 running = True
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False  # Set running to False to end the while loop.
-            elif event.key == pygame.K_LEFT:
-                pass
-            elif event.key == pygame.K_RIGHT:
-                pass
-
+    running = handle_events()
     clock.tick(165)
