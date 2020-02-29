43f4ff45189c081352919d29b626ff0d8b32ace6
Andrew Houghton <houghtonandrew0@gmail.com>
2020-02-29 18:15:19 +1100

    Switch to OO style

--- pygame/interface.py
+++ pygame/interface.py
@@ -3,40 +3,47 @@ from pathlib import Path
 from PIL import Image
 
 
-def get_image(filename):
-    image = Image.open(Path(__file__).absolute().parent.parent.joinpath("data", filename))
-    return pygame.image.fromstring(image.tobytes(), image.size, image.mode)
+class GameState:
+    def __init__(self):
+        pygame.init()
+        infoObject = pygame.display.Info()
+        size = (infoObject.current_w, infoObject.current_h)
+        print(f"Creating fullscreen window {size}")
+        self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 
+        self.clock = pygame.time.Clock()
 
-def handle_events():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                return False
-            elif event.key == pygame.K_LEFT:
-                pass
-            elif event.key == pygame.K_RIGHT:
-                pass
-    return True
+        # Set startup state
+        true_colour = self.get_image("true_colour_resized.jpg")
+        ballrect = true_colour.get_rect()
+        self.screen.blit(true_colour, ballrect)
+        pygame.display.flip()
 
-pygame.init()
-infoObject = pygame.display.Info()
-size = (infoObject.current_w, infoObject.current_h)
+        self.running = True
 
-screen = pygame.display.set_mode(size)
-pygame.display.set_mode(size, pygame.FULLSCREEN)
+    @staticmethod
+    def get_image(filename):
+        image = Image.open(Path(__file__).absolute().parent.parent.joinpath("data", filename))
+        return pygame.image.fromstring(image.tobytes(), image.size, image.mode)
 
-clock = pygame.time.Clock()
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.running = False
+                elif event.key == pygame.K_LEFT:
+                    pass
+                elif event.key == pygame.K_RIGHT:
+                    pass
 
-# Set startup state
-true_colour = get_image("true_colour_resized.jpg")
-ballrect = true_colour.get_rect()
-screen.blit(true_colour, ballrect)
-pygame.display.flip()
+    def main_loop(self):
+        pygame.display.update()
+        while self.running:
+            self.handle_events()
+            self.clock.tick(165)
 
-running = True
-while running:
-    running = handle_events()
-    clock.tick(165)
+if __name__ == "__main__":
+    game = GameState()
+    game.main_loop()
