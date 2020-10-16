6ddad8582ff79b6e987521907a37204d7ecedab1
Andrew Houghton <houghtonandrew0@gmail.com>
2020-10-16 12:09:32 +1100

    Close files afterwards

--- server/app.py
+++ server/app.py
@@ -10,22 +10,17 @@ import io
 
 
 app = Flask(__name__)
+
 heights_tif_path = (
     Path(__name__).absolute().parent.parent.joinpath("tasmania", "heights.tif")
 )
-heights_src = rio.open(heights_tif_path)
 colour_tif_path = (
     Path(__name__).absolute().parent.parent.joinpath("tasmania", "colour.tif")
 )
-colour_src = rio.open(colour_tif_path)
 CANVAS_WIDTH = 30978
 CANVAS_HEIGHT = 30978
-
-# heights_data = heights_src.read(1)
-# heights_data -= heights_data.min()
-# max_height_value = heights_data.max()
-min_height_value = -86
-max_height_value = 1610
+# min_height_value = -86
+# max_height_value = 1610
 
 
 @app.route("/")
@@ -39,22 +34,23 @@ def hello_world():
 )
 @app.route("/colour/<int:width>/<int:height>/<int:left_offset>/<int:top_offset>")
 def colour(width, height, left_offset, top_offset):
-    if left_offset is None:
-        left_offset = randint(0, CANVAS_WIDTH - width - 1)
-    if top_offset is None:
-        top_offset = randint(0, CANVAS_HEIGHT - height - 1)
-    w = rio.windows.Window(left_offset, top_offset, width, height)
+    with rio.open(colour_tif_path) as colour_src:
+        if left_offset is None:
+            left_offset = randint(0, CANVAS_WIDTH - width - 1)
+        if top_offset is None:
+            top_offset = randint(0, CANVAS_HEIGHT - height - 1)
+        w = rio.windows.Window(left_offset, top_offset, width, height)
 
-    rgbArray = np.zeros((width, height, 3), "uint8")
-    rgbArray[..., 0] = colour_src.read(1, window=w)
-    rgbArray[..., 1] = colour_src.read(2, window=w)
-    rgbArray[..., 2] = colour_src.read(3, window=w)
+        rgbArray = np.zeros((width, height, 3), "uint8")
+        rgbArray[..., 0] = colour_src.read(1, window=w)
+        rgbArray[..., 1] = colour_src.read(2, window=w)
+        rgbArray[..., 2] = colour_src.read(3, window=w)
 
-    file_object = io.BytesIO()
-    pil_img = Image.fromarray(rgbArray)
-    pil_img.save(file_object, "PNG")
-    file_object.seek(0)
-    return send_file(file_object, mimetype="image/png")
+        file_object = io.BytesIO()
+        pil_img = Image.fromarray(rgbArray)
+        pil_img.save(file_object, "PNG")
+        file_object.seek(0)
+        return send_file(file_object, mimetype="image/png")
 
 
 @app.route(
@@ -63,26 +59,27 @@ def colour(width, height, left_offset, top_offset):
 )
 @app.route("/height_image/<int:width>/<int:height>/<int:left_offset>/<int:top_offset>")
 def height_image(width, height, left_offset, top_offset):
-    if left_offset is None:
-        left_offset = randint(0, CANVAS_WIDTH - width - 1)
-    if top_offset is None:
-        top_offset = randint(0, CANVAS_HEIGHT - height - 1)
-    w = rio.windows.Window(left_offset, top_offset, width, height)
+    with rio.open(heights_tif_path) as heights_src:
+        if left_offset is None:
+            left_offset = randint(0, CANVAS_WIDTH - width - 1)
+        if top_offset is None:
+            top_offset = randint(0, CANVAS_HEIGHT - height - 1)
+        w = rio.windows.Window(left_offset, top_offset, width, height)
 
-    file_object = io.BytesIO()
-    height_values = heights_src.read(1, window=w)
-    min_height = height_values.min()
+        file_object = io.BytesIO()
+        height_values = heights_src.read(1, window=w)
+        min_height = height_values.min()
 
-    # Rescale Heights
-    height_range = height_values.max() - min_height
-    height_values -= min_height
-    height_values = height_values / height_range * 255
-    height_values = height_values.astype("uint8")
+        # Rescale Heights
+        height_range = height_values.max() - min_height
+        height_values -= min_height
+        height_values = height_values / height_range * 255
+        height_values = height_values.astype("uint8")
 
-    pil_img = Image.fromarray(height_values)
-    pil_img.save(file_object, "PNG")
-    file_object.seek(0)
-    return send_file(file_object, mimetype="image/png")
+        pil_img = Image.fromarray(height_values)
+        pil_img.save(file_object, "PNG")
+        file_object.seek(0)
+        return send_file(file_object, mimetype="image/png")
 
 
 @app.route(
@@ -91,13 +88,14 @@ def height_image(width, height, left_offset, top_offset):
 )
 @app.route("/height/<int:width>/<int:height>/<int:left_offset>/<int:top_offset>")
 def height(width, height, left_offset, top_offset):
-    if left_offset is None:
-        left_offset = randint(0, CANVAS_WIDTH - width - 1)
-    if top_offset is None:
-        top_offset = randint(0, CANVAS_HEIGHT - height - 1)
-    w = rio.windows.Window(left_offset, top_offset, width, height)
-    height_values = heights_src.read(1, window=w)
-    return jsonify(height_values.tolist())
+    with rio.open(heights_tif_path) as heights_src:
+        if left_offset is None:
+            left_offset = randint(0, CANVAS_WIDTH - width - 1)
+        if top_offset is None:
+            top_offset = randint(0, CANVAS_HEIGHT - height - 1)
+        w = rio.windows.Window(left_offset, top_offset, width, height)
+        height_values = heights_src.read(1, window=w)
+        return jsonify(height_values.tolist())
 
 
 if __name__ == "__main__":
