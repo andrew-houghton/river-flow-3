4e77c25b0b62e60a2ae8514136d54c9722e91148
Andrew Houghton <houghtonandrew0@gmail.com>
2021-01-21 00:07:16 +1100

    Add some simple path following. there are bugs at the moment

--- trace_river/trace.py
+++ trace_river/trace.py
@@ -2,8 +2,9 @@ from pyproj import Proj
 import rasterio
 from pathlib import Path
 import rasterio as rio
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 from algorithms import convert_to_graph, flood_low_points
+from pprint import pprint
 
 
 proj_string = "+proj=utm +zone=55 +south +datum=WGS84 +units=m +no_defs"
@@ -48,17 +49,21 @@ def get_raster(window, map_type="height"):
     else:
         raise Exception("Not implemented")
 
+
+def find_centerpoint(node_key):
+    x = sum(i[0] for i in node_key)/len(node_key)
+    y = sum(i[1] for i in node_key)/len(node_key)
+    return x, y
+
+
 def enlarge_bounding_box_until_path_is_found(start_rowcol, end_rowcol, size_factors):
     window = generate_bounding_box(start_rowcol, end_rowcol, size_factors)
     start_window_rowcol = apply_window_to_rowcol(window, start_rowcol)
     end_window_rowcol = apply_window_to_rowcol(window, end_rowcol)
 
     height_raster = get_raster(window)
-    # pyplot.imshow(height_raster, cmap='winter')
-    # pyplot.show()
     graph = convert_to_graph(height_raster)
     graph = flood_low_points(graph, height_raster)
-    # from pprint import pprint
     # pprint(list(graph.keys())[0])
     # pprint(list(graph.values())[0])
 
@@ -72,15 +77,25 @@ def enlarge_bounding_box_until_path_is_found(start_rowcol, end_rowcol, size_fact
     # while distance to end is within a threshold then add current end point as a candidate
     # if distance ever goes back above the threshold then pick the best end candidate
     # if the path hits the end then exit early
-
-    path = [start_window_rowcol]
+    key_lookup = {k:key for key in graph.keys() for k in key}
+    path = [key_lookup[start_window_rowcol]]
     for i in range(100):
-        last_point = path[-1]
-        next_points = graph[last_point]
-        lowest_point = next_points[0]
-        
+        previous_point = path[-1]
+        next_points = graph[previous_point]
+        if len(next_points) == 0:
+            break
+        selected_point = min(next_points, key=lambda node_key: height_raster[node_key[0]])
+        path.append(selected_point)
 
-    return trace_path(height_raster, start_window_rowcol, end_window_rowcol)
+    plt.imshow(height_raster, cmap='winter')
+    for i in range(len(path)-1):
+        current_point = find_centerpoint(path[i])
+        next_point = find_centerpoint(path[i+1])
+        print(current_point)
+        plt.plot(current_point, next_point)
+    plt.show()
+
+    # return trace_path(height_raster, start_window_rowcol, end_window_rowcol)
 
 def generate_bounding_box(start_rowcol, end_rowcol, size_factors):
     # Generate a box which encloses the start and end point
@@ -101,4 +116,4 @@ def generate_bounding_box(start_rowcol, end_rowcol, size_factors):
 if __name__ == '__main__':
     start_point = (147.086086,-41.448218)
     end_point = (147.122328,-41.443116)
-    print(start_finish_to_path(start_point, end_point))
+    start_finish_to_path(start_point, end_point)
